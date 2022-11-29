26e65c697075a5d2ada6d40d48067d9c2d2721e7
aloha4you <47893318+aloha4you@users.noreply.github.com>
2022-11-29 19:39:47 +0300

    add test for discounts
    push to master directly

--- shop/tests/test_views.py
+++ shop/tests/test_views.py
@@ -1,5 +1,8 @@
-from django.test import TestCase, Client
-from shop.views import PurchaseCreate
+from django.test import TestCase
+from django.test import Client
+
+from shop.models import Discount
+
 
 class PurchaseCreateTestCase(TestCase):
     def setUp(self):
@@ -20,3 +23,15 @@ class DiscountCreateTestCase(TestCase):
         def test_webpage_not_found(self):
             response = self.client.get('discount/')
             self.assertEqual(response.status_code, 404)
+
+        def test_discount_registrate(self):
+            self.client.post('/discount/',
+                                        data={
+                                              "total" : "0",
+                                              "discount" : "0",
+                                              "person" : "test1"})
+
+            self.assertEqual("test1", Discount.objects.first().person)
+            self.assertEqual(0, Discount.objects.first().discount)
+            self.assertEqual(0, Discount.objects.first().total)
+
